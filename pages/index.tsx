ea1081af60990ae56918bd27102e5e7f24e7222a
Umair Abdullah <talhaejaz@outlook.com>
2022-11-20 15:48:51 +0500

    Startup

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Personal Website</title>
+        <meta name="description" content="My home on internet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et error quia
+        consectetur cum. Omnis sunt nulla fugit laudantium non, eum tempore
+        voluptatem atque nemo magnam esse exercitationem voluptate reiciendis
+        impedit, consequatur amet at vero ab beatae laboriosam ducimus corrupti
+        saepe officiis. Nostrum quas tempore totam asperiores. Quis temporibus
+        dolore saepe.
+      </h1>
+    </div>
+  );
+}
