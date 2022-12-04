b1d1f00850991d74b299bc2d450703616b4872ea
Umair Abdullah <talhaejaz@outlook.com>
2022-12-04 19:35:40 +0500

    logo done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 import styles from "../styles/componentStyles/Header.module.scss";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h2>
-        <span>T</span>alha <span>E</span>jaz
-      </h2>
+      <Link href="/">
+        <h2>
+          <span>T</span>alha <span>E</span>jaz
+        </h2>
+      </Link>
     </header>
   );
 };
