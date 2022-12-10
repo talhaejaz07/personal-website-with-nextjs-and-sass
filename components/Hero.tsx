85a8ad407e074c15371af7cfb855c4567aa6d0f9
Talha Ejaz <talhaejaz@outlook.com>
2022-12-11 01:12:58 +0500

    hero complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,13 +8,19 @@ export const Hero = () => {
     <section className={styles.hero}>
       <div>
         <h3>Hello!</h3>
-        <h1>I am Talha Ejaz</h1>
+        <h1>
+          I am <span>T</span>alha <span>E</span>jaz
+        </h1>
         <p>
           An engineer who loves to create and has over a decade of experience.
         </p>
       </div>
 
-      <Image src={portrait} alt="Portrait" objectFit="contain" />
+      <Image
+        src={portrait}
+        alt="Portrait"
+        sizes="(max-width: 550px) calc(100vw - 32px), min(calc(50vw - 24px), (600px - 24px))"
+      />
     </section>
   );
 };
